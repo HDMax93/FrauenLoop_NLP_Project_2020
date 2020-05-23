8e5e21f902d42e7b184e2c1d03956fde5351bc5b
HDMax93 <henriekemax@googlemail.com>
2020-05-23 18:42:25 +0200

    Update API code to retrieve Crunchbase data

--- data/raw/Crunchbase_API2.py
+++ data/raw/Crunchbase_API2.py
@@ -6,23 +6,23 @@ import random
 from ratelimit import limits, RateLimitException
 from backoff import on_exception, expo
 
-"""
+
 FIFTEEN_MINUTES = 900
 
 @on_exception(expo, RateLimitException, max_tries=8)
 @limits(calls=15, period=FIFTEEN_MINUTES)
-sys.path
-def call_asetpi(url, params):
+def call_api(url, params):
     response = requests.get(url, params)
 
     if response.status_code != 200:
         raise Exception('API response: {}'.format(response.status_code))
     return response
-"""
+
 
 url = "https://api.crunchbase.com/v3.1/organizations"
 querystring = {"locations":"Berlin","organization_types":"company","sort_order":"created_at DESC","page":"1","user_key":"e3df6582c6b0b66ca02d192734ae8b37"}
 
+call_api(url, params = querystring)
 
 """
 # Load credentials from json file
@@ -30,10 +30,11 @@ with open("crunchbase_credentials.json", "r") as file:
     creds = json.load(file)
 """
 
+"""
 response = requests.request("GET", url, params=querystring)
 
 print(response.text)
-
+"""
 
 """ 
 --Body Request--
